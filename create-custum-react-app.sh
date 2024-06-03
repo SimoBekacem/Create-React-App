25db6ac1b6589d86744f9a0dde2ed9b71544fcf7
SimoBekacem <SimoBekacem>
2024-06-03 19:06:06 +0100

    adding the redux toolkit commands in create-custum-react-app.sh and change the location of scss folder inside the src and finaly update the create-custum-react-app.sh to be more readabel

--- create-custum-react-app.sh
+++ create-custum-react-app.sh
@@ -1,33 +1,43 @@
-!#/bin/bash 
+#!/bin/bash
 
-echo "By default the project folder will created courant diroctory."
-# Getting he name of the app from the user.
-read -p "Give me the name of the project: " AppName 
-# Creating a directory with the name of the App.
-mkdir "$(pwd)/$AppName"
-# Change the working dir to AppName dir
-cd "$(pwd)/$AppName" 
-# Initiating the npm package
+# Welcome message and information
+echo "By default, the project folder will be created in the current directory."
+
+# Getting the name of the app from the user
+read -p "Give me the name of the project: " AppName
+
+# Creating a directory with the name of the app and navigating into it
+mkdir -p "$AppName"
+cd "$AppName"
+
+# Initializing the npm package
 npm init -y
-# Installing the react packges 
-npm install react
-npm install react-dom
-npm install webpack --save-dev
-npm install webpack-cli --save-dev
-npm install webpack-dev-server --save-dev
-npm install @babel/core --save-dev
-npm install babel-loader --save-dev
-npm install @babel/preset-react --save-dev
-npm install @babel/preset-env --save-dev
-npm install html-webpack-plugin --save-dev
-# Now we will copy the webpack config file from here to the AppName dir
-cp -r ../React/webpack.config.js ./
-# Now we will copy the src folder to the AppName folder
-cp -r ../React/src ./
-# Copy the sass template to the project
-cp -r ../scss ./src/
-# ask the user to add the scripts start and build.
-echo " Add this scripts \"start\":\"webpack-dev-server --mode development --open --hot\" to package.json"
-echo " Add this scripts \"build\":\"webpack --mode production\" to package.json "
-echo "I'am Done, happy codding :-)"
 
+# Installing React and related packages
+npm install react react-dom
+
+# Installing webpack and related packages
+npm install --save-dev webpack webpack-cli webpack-dev-server
+
+# Installing Babel and related packages
+npm install --save-dev @babel/core babel-loader @babel/preset-react @babel/preset-env
+
+# Installing HTML Webpack Plugin
+npm install --save-dev html-webpack-plugin
+
+# Copying the webpack config file from the parent React directory to the project directory
+cp -r ../React/webpack.config.js ./
+
+# Copying the src folder from the parent React directory to the project directory
+cp -r ../React/src ./
+
+# Displaying instructions for adding scripts to package.json
+echo 'Add the following scripts to your package.json:'
+echo '"start": "webpack-dev-server --mode development --open --hot",'
+echo '"build": "webpack --mode production"'
+
+# Installing Redux Toolkit and React-Redux packages
+npm install @reduxjs/toolkit react-redux
+
+# Completion message
+echo "I'm done. Happy coding! :-)"
